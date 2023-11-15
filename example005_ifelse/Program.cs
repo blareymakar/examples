31b1cf4f6f430957d8dc7d2a485e4521a2fdfc73
Andrey Makarov <mav737@mail.ru>
2023-11-15 23:20:58 +0300

    add example005_ifelse condition

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower()=="maria")
+{
+    Console.WriteLine("ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
+
